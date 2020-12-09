45f2a1ea21bd329cc216c91b7fb4668646772276
daifei <806574913@qq.com>
2020-12-09 10:36:55 +0800

    初始化项目

---
+++ src/main/main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+
+function createWindow(){
+  const url = path.join(__dirname,'../../dist/index.html')
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+  // mainWindow.loadURL('http://localhost:3000/')
+  mainWindow.loadFile(url)
+}
+
+
+
+app.on('ready', function () {
+  createWindow()
+})
+app.on('browser-window-blur', (e) => {
+  console.log('blur')
+})
+app.on('browser-window-focus', (e) => {
+  console.log('focus')
+})
+app.on('before-quit', function (e) {
+  console.log('out');
+})
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+       app.quit();
+  }
+})
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+//热更新
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { join } from 'path'
+
+
+const config = {
+  root: join(__dirname,'./src/render'),
+  base: './',
+  outDir: join(__dirname,'./dist')
+}
+
+export default config
